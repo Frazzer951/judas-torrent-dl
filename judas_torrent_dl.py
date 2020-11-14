516a4ac0cc457bc3e15559b434180596cd91cc93
Luke Eltiste <lukeeltiste@csu.fullerton.edu>
2020-11-14 12:36:33 -0800

    Formatting and Time changes

--- judas_torrent_dl.py
+++ judas_torrent_dl.py
@@ -6,28 +6,33 @@ import time
 from pathlib import Path
 
 link_regex = re.compile(
-    r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))")
-tor_regex = re.compile(r'https://nyaa.si/download/(\d+).torrent')
+    r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+)
+tor_regex = re.compile(r"https://nyaa.si/download/(\d+).torrent")
 
-base_folder = Path('downloads')
+base_folder = Path("downloads")
 
-while(True):
-    d = feedparser.parse('https://nyaa.si/?page=rss&u=Judas')
+while True:
+    d = feedparser.parse("https://nyaa.si/?page=rss&u=Judas")
 
     links = []
 
-    for link in link_regex.findall(str(d['entries'])):
-        if('nyaa.si/download' in link[0]):
+    for link in link_regex.findall(str(d["entries"])):
+        if "nyaa.si/download" in link[0]:
             links.append(link[0])
 
     for link in links:
         filename = tor_regex.search(link).group(1)
 
-        if not (path.exists(base_folder/(filename+'.torrent')) or path.exists(base_folder/(filename+'.torrent.added')) or path.exists(base_folder/(filename+'.torrent.invalid'))):
-            print('Downloading')
+        if not (
+            path.exists(base_folder / (filename + ".torrent"))
+            or path.exists(base_folder / (filename + ".torrent.added"))
+            or path.exists(base_folder / (filename + ".torrent.invalid"))
+        ):
+            print("Downloading")
             r = requests.get(link, allow_redirects=True)
-            open(base_folder/(filename+'.torrent'), 'wb').write(r.content)
+            open(base_folder / (filename + ".torrent"), "wb").write(r.content)
         else:
-            print('File Already Exists')
-    print('checking again in 24 hour')
-    time.sleep(86400)
+            print("File Already Exists")
+    print("checking again in 1 hour")
+    time.sleep(3600)
---
+++ readme.md
@@ -0,0 +1,3 @@
+#### This program will look at the RSS feed provided by:
+`https://nyaa.si/?page=rss&u=Judas`
+#### And will grab all the links that download torrent files and download them. The downloaded torrents will be saves to the '/downloads' folder, where you can add them to what ever downloader you want
