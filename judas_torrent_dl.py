07fc5e4ae11c3cd9f7ab75a308b710c5fffb6ad5
Luke Eltiste <lukeeltiste@csu.fullerton.edu>
2020-12-03 18:01:28 -0800

    change print to log

--- judas_torrent_dl.py
+++ judas_torrent_dl.py
@@ -5,6 +5,11 @@ import os
 from os import path
 import time
 from pathlib import Path
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 link_regex = re.compile(
     r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
@@ -30,12 +35,12 @@ while True:
             or path.exists(base_folder / (filename + ".torrent.added"))
             or path.exists(base_folder / (filename + ".torrent.invalid"))
         ):
-            print("Downloading " + link)
+            logging.info("Downloading " + link)
             r = requests.get(link, allow_redirects=True)
             if not os.path.exists(base_folder):
                 os.makedirs(base_folder)
             open(base_folder / (filename + ".torrent"), "wb").write(r.content)
         # else:
         #    print("File Already Exists")
-    print("Checking again in 1 hour")
+    logging.info("Checking again in 1 hour")
     time.sleep(3600)
